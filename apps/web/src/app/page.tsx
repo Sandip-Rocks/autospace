10b06cbd92a222c8ec540fb94b0b580f72436ea6
Sandip Das <sandip4991@gmail.com>
2024-08-26 22:45:08 +0530

    fix: lint

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,5 @@
-import { add } from "@autospace/sample-lib";
+import { add } from '@autospace/sample-lib'
 
 export default function Home() {
-  return (
-    <main>Hello {add(2, 2)}</main>
-  );
+  return <main>Hello {add(2, 2)}</main>
 }
