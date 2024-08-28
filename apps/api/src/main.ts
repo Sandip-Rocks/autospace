49776a35a0bab5d3c0438da16da6901ecbc784b9
Sandip Das <sandip4991@gmail.com>
2024-08-28 19:37:03 +0530

    Add Swagger for REST API

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
+  const config = new DocumentBuilder()
+    .setTitle('ParkPro | Sandip Das')
+    .setDescription(
+      `The ParkPro API description
+      <h2>Click on this link to open <a href="https://studio.apollographql.com/sandbox/explorer">Apollo Graphql Playground</a> </h2>`,
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/', app, document)
   await app.listen(3000)
 }
 bootstrap()
